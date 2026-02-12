3f5fa33581b7f06d39cbab88062def98253b3109
DHH.COG <110273797+HoangDang119763@users.noreply.github.com>
2026-02-13 00:31:31 +0700

    Invoice Import UI

--- src/main/java/GUI/InvoiceController.java
+++ src/main/java/GUI/InvoiceController.java
@@ -1,13 +1,10 @@
 package GUI;
 
 import BUS.*;
-import DTO.DetailDiscountDTO;
 import DTO.DetailInvoiceDTO;
-import DTO.DiscountDTO;
 import DTO.InvoiceDTO;
 import INTERFACE.IController;
 import SERVICE.PrintService;
-import SERVICE.SessionManagerService;
 import UTILS.NotificationUtils;
 import UTILS.UiUtils;
 import UTILS.ValidationUtils;
@@ -19,9 +16,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 
-import java.math.BigDecimal;
-
-public class InvoiceController implements IController  {
+public class InvoiceController implements IController {
     @FXML
     private TableView<InvoiceDTO> tblInvoice;
     @FXML
@@ -33,11 +28,9 @@ public class InvoiceController implements IController  {
     @FXML
     private TableColumn<InvoiceDTO, Integer> tlb_col_customerId;
     @FXML
-    private TableColumn<InvoiceDTO, String> tlb_col_discountCode;
-    @FXML
-    private TableColumn<InvoiceDTO, String> tlb_col_discountAmount;
-    @FXML
     private TableColumn<InvoiceDTO, String> tlb_col_totalPrice;
+    @FXML
+    private TableColumn<InvoiceDTO, String> tlb_col_status;
     @FXML
     private TableView<DetailInvoiceDTO> tblDetailInvoice;
     @FXML
@@ -49,91 +42,105 @@ public class InvoiceController implements IController  {
     @FXML
     private TableColumn<DetailInvoiceDTO, String> tlb_col_totalPriceP;
     @FXML
-    private Label id;
+    private TextField id;
 
     @FXML
-    private Label createDate;
+    private TextField createDate;
 
     @FXML
-    private Label employeeId;
+    private TextField employeeId;
 
     @FXML
-    private Label customerId;
+    private TextField customerId;
 
     @FXML
-    private Label discountCode;
+    private TextField discountCode;
 
     @FXML
-    private Label discountAmount;
+    private TextField discountAmount;
 
     @FXML
-    private Label totalPrice;
+    private TextField totalPrice;
+
+    @FXML
+    private TextField status;
+    @FXML
+    private HBox functionBtns;
     @FXML
     private Button exportPdf;
     @FXML
-    private Button refreshBtn;
-    @FXML
-    private Button advanceSearchBtn;
+    private Button advanceSearchBtn, refreshBtn;
     @FXML
     private TextField txtSearch;
     private String keyword = "";
     private InvoiceDTO selectedInvoice;
+    private InvoiceBUS invoiceBUS;
+    private DetailInvoiceBUS detailInvoiceBUS;
+    private StatusBUS statusBUS;
 
     @FXML
     public void initialize() {
-        if (InvoiceBUS.getInstance().isLocalEmpty()) InvoiceBUS.getInstance().loadLocal();
-        if (DetailInvoiceBUS.getInstance().isLocalEmpty()) DetailInvoiceBUS.getInstance().loadLocal();
+        invoiceBUS = InvoiceBUS.getInstance();
+        detailInvoiceBUS = DetailInvoiceBUS.getInstance();
+        statusBUS = StatusBUS.getInstance();
+        if (invoiceBUS.isLocalEmpty())
+            invoiceBUS.loadLocal();
+        if (detailInvoiceBUS.isLocalEmpty())
+            detailInvoiceBUS.loadLocal();
+        if (statusBUS.isLocalEmpty())
+            statusBUS.loadLocal();
         tblInvoice.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
-//        tblDetailInvoice.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
         Platform.runLater(() -> tblInvoice.getSelectionModel().clearSelection());
         Platform.runLater(() -> tblDetailInvoice.getSelectionModel().clearSelection());
-
 
         hideButtonWithoutPermission();
         setupListeners();
 
         loadTable();
     }
+
     @Override
     public void loadTable() {
         ValidationUtils validationUtils = ValidationUtils.getInstance();
         tlb_col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        tlb_col_createDate.setCellValueFactory(cellData ->
-                formatCell(validationUtils.formatDateTimeWithHour(cellData.getValue().getCreateDate())));
+        tlb_col_createDate.setCellValueFactory(
+                cellData -> formatCell(validationUtils.formatDateTimeWithHour(cellData.getValue().getCreateDate())));
         tlb_col_employeeId.setCellValueFactory(new PropertyValueFactory<>("employeeId"));
         tlb_col_customerId.setCellValueFactory(new PropertyValueFactory<>("customerId"));
-        tlb_col_discountCode.setCellValueFactory(new PropertyValueFactory<>("discountCode"));
-        tlb_col_discountAmount.setCellValueFactory(cellData ->
-                formatCell(validationUtils.formatCurrency(cellData.getValue().getDiscountAmount())));
-        tlb_col_totalPrice.setCellValueFactory(cellData ->
-                formatCell(validationUtils.formatCurrency(cellData.getValue().getTotalPrice())));
+
+        tlb_col_totalPrice.setCellValueFactory(
+                cellData -> formatCell(validationUtils.formatCurrency(cellData.getValue().getTotalPrice())));
+        tlb_col_status.setCellValueFactory(cellData -> new SimpleStringProperty(statusBUS
+                .getByIdLocal(cellData.getValue().getStatusId()).getDescription()));
         UiUtils.gI().addTooltipToColumn(tlb_col_createDate, 10);
-        UiUtils.gI().addTooltipToColumn(tlb_col_discountAmount, 10);
         UiUtils.gI().addTooltipToColumn(tlb_col_totalPrice, 10);
-        tblInvoice.setItems(FXCollections.observableArrayList(InvoiceBUS.getInstance().getAllLocal()));
+        tblInvoice.setItems(FXCollections.observableArrayList(invoiceBUS.getAllLocal()));
     }
 
     public void loadSubTable(int invoiceId) {
-        if (invoiceId <= 0) return;
+        if (invoiceId <= 0)
+            return;
         ValidationUtils validationUtils = ValidationUtils.getInstance();
         this.id.setText(String.valueOf(selectedInvoice.getId()));
         this.createDate.setText(validationUtils.formatDateTime(selectedInvoice.getCreateDate()));
         this.employeeId.setText(String.valueOf(selectedInvoice.getEmployeeId()));
         this.customerId.setText(String.valueOf(selectedInvoice.getCustomerId()));
-        this.discountCode.setText(selectedInvoice.getDiscountCode() != null ? selectedInvoice.getDiscountCode() : "Không có");
+        this.discountCode
+                .setText(selectedInvoice.getDiscountCode() != null ? selectedInvoice.getDiscountCode() : "");
         this.discountAmount.setText(validationUtils.formatCurrency(selectedInvoice.getDiscountAmount()));
         this.totalPrice.setText(validationUtils.formatCurrency(selectedInvoice.getTotalPrice()));
-
+        this.status.setText(statusBUS.getByIdLocal(selectedInvoice.getStatusId()).getDescription());
         tlb_col_productId.setCellValueFactory(new PropertyValueFactory<>("productId"));
         tlb_col_quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
-        tlb_col_price.setCellValueFactory(cellData ->
-                formatCell(validationUtils.formatCurrency(cellData.getValue().getPrice())));
-        tlb_col_totalPriceP.setCellValueFactory(cellData ->
-                formatCell(validationUtils.formatCurrency(cellData.getValue().getTotalPrice())));
+        tlb_col_price.setCellValueFactory(
+                cellData -> formatCell(validationUtils.formatCurrency(cellData.getValue().getPrice())));
+        tlb_col_totalPriceP.setCellValueFactory(
+                cellData -> formatCell(validationUtils.formatCurrency(cellData.getValue().getTotalPrice())));
 
         UiUtils.gI().addTooltipToColumn(tlb_col_price, 10);
         UiUtils.gI().addTooltipToColumn(tlb_col_totalPriceP, 10);
-        tblDetailInvoice.setItems(FXCollections.observableArrayList(DetailInvoiceBUS.getInstance().getAllDetailInvoiceByInvoiceIdLocal(invoiceId)));
+        tblDetailInvoice.setItems(FXCollections
+                .observableArrayList(detailInvoiceBUS.getAllDetailInvoiceByInvoiceIdLocal(invoiceId)));
         tblDetailInvoice.getSelectionModel().clearSelection();
     }
 
@@ -154,7 +161,7 @@ public class InvoiceController implements IController  {
         txtSearch.textProperty().addListener((observable, oldValue, newValue) -> handleKeywordChange());
         refreshBtn.setOnAction(event -> {
             resetFilters();
-            NotificationUtils.showInfoAlert("L+�m m�+�i th+�nh c+�ng", "Th+�ng b+�o");
+            NotificationUtils.showInfoAlert("Làm mới thành công", "Thông báo");
         });
         exportPdf.setOnAction(e -> handleExportPDF());
         advanceSearchBtn.setOnAction(e -> handleAdvanceSearch());
@@ -188,8 +195,7 @@ public class InvoiceController implements IController  {
             try {
                 int id = Integer.parseInt(keyword);
                 tblInvoice.setItems(FXCollections.observableArrayList(
-                        invoiceBUS.getByIdLocal(id)
-                ));
+                        invoiceBUS.getByIdLocal(id)));
             } catch (NumberFormatException e) {
                 // X�+� l++ tr���+�ng h�+�p kh+�ng phߦ�i s�+�
                 tblInvoice.setItems(FXCollections.observableArrayList());
@@ -214,8 +220,7 @@ public class InvoiceController implements IController  {
         InvoiceAdvanceSearchModalController modalController = UiUtils.gI().openStageWithController(
                 "/GUI/InvoiceAdvanceSearchModal.fxml",
                 null,
-                "Tìm kiếm nâng cao"
-        );
+                "Tìm kiếm nâng cao");
         if (modalController != null && modalController.isSaved()) {
             tblInvoice.setItems(FXCollections.observableArrayList(modalController.getFilteredInvoices()));
             tblInvoice.getSelectionModel().clearSelection();
